d86b4ac0e2a79596f144fe72e56e0be5a355e72b
Tysiak Anastasiia web2025-5 <tysiaknastia@gmail.com>
2025-03-30 16:46:58 +0300

    Added GET to cat.js

--- cat.js
+++ cat.js
@@ -1,29 +1,45 @@
 const http = require('http'); 
 const { Command } = require('commander'); 
 const fs = require('fs').promises; 
-const path = require ('path'); 
-const superagent = require ('superagent');
+const path = require('path'); 
 
 const program = new Command(); 
 program
-.requiredOption('-h, --host <host>', 'Server host' )
-.requiredOption('-p, --port <port>', 'Server port' )
-.requiredOption('-c, --cache <cache>', 'Cache directory');
+    .requiredOption('-h, --host <host>', 'Server host')
+    .requiredOption('-p, --port <port>', 'Server port')
+    .requiredOption('-c, --cache <cache>', 'Cache directory');
 
 try {
-program.parse(process.argv); 
-const options = program.opts();
-const { host, port, cache } = options; 
+    program.parse(process.argv);
+    const options = program.opts();
+    const { host, port, cache } = options;
 
-const server = http.createServer ((req, res) => {
-    res.writeHead (200, {'Content-Type' : 'text/plain' }); 
-    res.end('Server is running\n'); 
-});
+    const server = http.createServer(async (req, res) => {
+        const code = req.url.slice(1);
 
-server.listen (port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
-    console.log(`Cache directory: ${options.cache}`);
-});
+        if (req.method === 'GET') {
+            try {
+                const filePath = path.join(cache, `${code}.jpg`);
+                await fs.access(filePath);
+
+                const image = await fs.readFile(filePath);
+                res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+                res.end(image);
+            } catch (error) {
+                res.writeHead(404, { 'Content-Type': 'text/plain' });
+                res.end('Image not found');
+            }
+        } else {
+            res.writeHead(405, { 'Content-Type': 'text/plain' });
+            res.end('Method Not Allowed');
+        }
+    });
+
+    server.listen(port, host, () => {
+        console.log(`Server is running on http://${host}:${port}`);
+        console.log(`Cache directory: ${cache}`);
+    });
+
 } catch (error) {
     console.error(error.message);
     process.exit(1);
